7240b95d19e3251cd13fc66034344ed1552c9fc3
EmilioRivera0 <elcholula619@gmail.com>
2023-09-29 15:38:03 -0600

    Initial Upload

---
+++ get_functions_identifiers.py
@@ -0,0 +1,8 @@
+# necessary libraries -------->
+import re
+
+# functions definition -------->
+def generate_func_ids_array():
+    func_ids_array = []
+    
+    return func_ids_array
---
+++ main.py
@@ -0,0 +1,5 @@
+# necessary libraries -------->
+from .get_functions_identifiers import generate_func_ids_array
+
+# program execution -------->
+
